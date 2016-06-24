c12daadcb0886d27cd23599bf4f07347c008f57b
Mark Murnane <mark@hackafe.net>
2016-06-24 15:13:00 -0400

    Added blob detection

--- blob.py
+++ blob.py
@@ -1,31 +1,33 @@
-#!/usr/bin/python
+#!/usr/local/bin/python
 # Standard imports
 import cv2
 import numpy as np;
 
-
 cap = cv2.VideoCapture(0)
-fgbg = cv2.createBackgroundSubtractorMOG2()
+#cap = cv2.VideoCapture('test.avi')
+fgbg = cv2.BackgroundSubtractorMOG2(10, 5, False)
+ret, base = cap.read()
 
 while(True):
-    # Capture frame-by-frame
     ret, frame = cap.read()
+    detector = cv2.SimpleBlobDetector()
+    fgmask = fgbg.apply(frame)
+    diff = np.zeros((720,1280,3), np.uint8)
+    cv2.absdiff(base, frame, diff)
+    fgmask = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    ret, fgmask = cv2.threshold(fgmask, 15, 255, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(fgmask.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Set up the detector with default parameters.
-    detector = cv2.SimpleBlobDetector_create()
- 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Detect blobs.
-    fgmask = fgbg.apply(gray)
-    keypoints = detector.detect(fgmask)
- 
-    # Draw detected blobs as red circles.
-    # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-    im_with_keypoints = cv2.drawKeypoints(frame, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    bigContours = []
+    for contour in contours:
+        if cv2.contourArea(contour) >= 1000:
+            bigContours.append(contour)
 
-    print([(x.pt, x.size, x.angle) for x in keypoints])
-    # Display the resulting frame
-    cv2.imshow('frame',im_with_keypoints)
+    vis = np.zeros((720,1280,3), np.uint8)
+    contour_image = cv2.drawContours(frame, bigContours, -1, (255, 0, 0), 1)
+
+    cv2.imshow('frame',frame)
+    cv2.imshow('mask',fgmask)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
  
